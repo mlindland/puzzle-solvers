947a2f570428bbc36eba8c470080a72b845b406a
magnus lindland <lindlandm@cs.unm.edu>
2021-11-23 15:16:44 -0700

    Refactoring

---
+++ move-sliding-tile.pl
@@ -0,0 +1,148 @@
+:- use_module(library(listing)).
+
+% test2 :- heuristic([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0],
+%     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0], 
+%     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0],
+%     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0], H).
+
+writelist([]).
+writelist([N1, N2, N3, N4|Rest]) :-
+    format('[~w ~w ~w ~w]~n', [N1, N2, N3, N4]),
+    writelist(Rest).
+
+add(N1, N2, Result) :-
+    Result is N1+N2.
+
+swap_elements(List, I, J, Result) :-
+    same_length(List, Result),
+    append(BeforeI, [AtI|PastI], List),
+    append(BeforeI, [AtJ|PastI], List1),
+    append(BeforeJ, [AtJ|PastJ], List1),
+    append(BeforeJ, [AtI|PastJ], Result),
+    length(BeforeI, I),
+    length(BeforeJ, J).
+
+heuristic([], _, _, 0).
+heuristic([Head_state | Rest_state],State, Goal, H) :-
+    indexof(IState, Head_state, State),
+    indexof(IGoal, Head_state, Goal),
+    get_x_y(IState, I, J),
+    get_x_y(IGoal, R, C),
+    DeltaX is C - J,
+    DeltaY is R - I,
+    abs(DeltaX, X),
+    abs(DeltaY, Y),
+    heuristic(Rest_state, State, Goal, Remaining),
+    H is X + Y + Remaining.
+
+get_x_y(Index, I, J) :-
+    I is div(Index, 4),
+    J is mod(Index, 4).
+
+indexof(Index, Element, List) :-
+    nth0(Index1, List, Element),
+    Index is Index1.
+
+combinations(Goal, Result) :- 
+    indexof(Index, 0, Goal),
+    length(Goal, R),
+    Index =:= R-1,
+    append([Goal], [], Result).
+combinations(Goal, Result) :-
+    append([Goal], Remaining, Result),
+    indexof(Index, 0, Goal),
+    add(Index, 1, Index1),
+    swap_elements(Goal, Index, Index1, Rest),
+    combinations(Rest, Remaining).
+movezerotofront(Goal, Result) :-
+    delete(Goal, 0, X),
+    append([0], X, Result).
+up(State) :-
+    indexof(I, 0, State),
+    I\==0,
+    indexof(I, 0, State),
+    I\==1,
+    indexof(I, 0, State),
+    I\==2,
+    indexof(I, 0, State),
+    I\==3.
+down(State) :-
+    indexof(I, 0, State),
+    I\==12,
+    indexof(I, 0, State),
+    I\==13,
+    indexof(I, 0, State),
+    I\==14,
+    indexof(I, 0, State),
+    I\==15.
+left(State) :-
+    indexof(I, 0, State),
+    I\==0,
+    indexof(I, 0, State),
+    I\==4,
+    indexof(I, 0, State),
+    I\==8,
+    indexof(I, 0, State),
+    I\==12.
+right(State) :-
+    indexof(I, 0, State),
+    I\==3,
+    indexof(I, 0, State),
+    I\==7,
+    indexof(I, 0, State),
+    I\==11,
+    indexof(I, 0, State),
+    I\==15.
+
+    % move left
+move(State, Next) :-
+    left(State),
+    indexof(Index, 0, State),
+    add(Index, -1, J),
+    swap_elements(State, Index, J, Next).
+    % write('-------------Start------------'), nl,
+    % writelist(State),
+    % write('try moving left'), nl,
+    % writelist(Next),
+    % write('--------------End-------------'), nl.
+
+    % move right
+move(State, Next) :-
+    right(State),
+    indexof(Index, 0, State),
+    add(Index, 1, J),
+    swap_elements(State, Index, J, Next).
+    % write('-------------Start------------'), nl,
+    % writelist(State),
+    % write('try moving right'), nl,
+    % writelist(Next), nl,
+    % write('--------------End-------------'), nl.
+
+    %move up
+move(State, Next) :-
+    up(State),
+    indexof(Index, 0, State),
+    add(Index, -4, J),
+    swap_elements(State, Index, J, Next).
+    % write('-------------Start------------'), nl,
+    % writelist(State),
+    % write('try moving up'), nl,
+    % writelist(Next), nl,
+    % write('--------------End-------------'), nl.
+
+    %move down
+move(State, Next) :-
+    down(State),
+    indexof(Index, 0, State),
+    add(Index, 4, J),
+    swap_elements(State, Index, J, Next).
+    % write('-------------Start------------'), nl,
+    % writelist(State),
+    % write('try moving down'), nl,
+    % writelist(Next), nl,
+    % write('--------------End-------------'), nl.
+
+
+% test2 :- 
+%     move([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0], Next),
+%     maplist(portray_clause, Next).
---
+++ move-vamp-wolf.pl
@@ -0,0 +1,69 @@
+% The first constant is how many vampires are on the right side of river, 
+% the second is how many warewolves are on the right size of the river.
+% i.e. unsafe_state(1,0) means there is 1 vampire on the right side, and 0 warewolves,
+% thus there are 2 vampires, and 3 warewolves on the leftside creating an unstable state.
+unsafe_state(1,0).  %Right = 1, Left = 0.
+unsafe_state(1,2).
+unsafe_state(1,3).
+unsafe_state(2,0).
+unsafe_state(2,1).
+unsafe_state(2,3).
+
+opp(1,0).
+opp(0,1).
+
+unsafe(state(N1,N2,N3,N4,N5,N6)) :- A is N1+N2+N3, B is N4+N5+N6, unsafe_state(A,B).
+
+writelist([]).
+writelist([X|Xs]) :- write(X), writelist(Xs), nl.
+heuristic(state(S1,S2,S3,S4,S5,S6,S7), state(G1,G2,G3,G4,G5,G6,G7), H) :- 
+    R1 is G1 - S1,
+    R2 is G2 - S2,
+    R3 is G3 - S3,
+    R4 is G4 - S4,
+    R5 is G5 - S5,
+    R6 is G6 - S6,
+    R7 is G7 - S7,
+    abs(R1, X1),
+    abs(R2, X2),
+    abs(R3, X3),
+    abs(R4, X4),
+    abs(R5, X5),
+    abs(R6, X6),
+    abs(R7, X7),
+    H is X1+X2+X3+X4+X5+X6+X7.
+
+move(state(X, V2, V3, X, W2, W3, X), state(Y, V2, V3, Y, W2, W3, Y)) :-
+    opp(X,Y), not(unsafe(state(Y, V2, V3, Y, W2, W3))),
+    writelist(['try V1 - W1 ',Y, V2, V3, Y, W2, W3, ' Boat:', Y]).
+
+move(state(X, X, V3, W1, W2, W3, X), state(Y, Y, V3, W1, W2, W3, Y)) :-
+    opp(X,Y), not(unsafe(state(Y, Y, V3, W1, W2, W3))),
+    writelist(['try V1 - V2 ',Y, Y, V3, W1, W2, W3, ' Boat:', Y]).
+
+move(state(X, V2, X, W1, W2, W3, X), state(Y, V2, Y, W1, W2, W3, Y)) :-
+    opp(X,Y), not(unsafe(state(Y, V2, Y, W1, W2, W3))),
+    writelist(['try V1 - V3 ',Y, V2, Y, W1, W2, W3, ' Boat:', Y]).
+
+move(state(V1, V2, V3, X, W2, X, X), state(V1, V2, V3, Y, W2, Y, Y)) :-
+    opp(X,Y), not(unsafe(state(V1, V2, V3, Y, W2, Y))),
+    writelist(['try W1 - W3 ',V1, V2, V3, Y, W2, Y, ' Boat:', Y]).
+
+move(state(V1, V2, V3, W1, X, X, X), state(V1, V2, V3, W1, Y, Y, Y)) :-
+    opp(X,Y), not(unsafe(state(V1, V2, V3, W1, Y, Y))),
+    writelist(['try W2 - W3 ',V1, V2, V3, W1, Y, Y, ' Boat:', Y]).
+
+move(state(X, V2, V3, W1, W2, W3, X), state(Y, V2, V3, W1, W2, W3, Y)) :-
+    opp(X,Y), not(unsafe(state(Y, V2, V3, W1, W2, W3))),
+    writelist(['try V1 rides alone ',Y, V2, V3, W1, W2, W3, ' Boat:', Y]).
+
+move(state(V1, V2, V3, W1, X, W3, X), state(V1, V2, V3, W1, Y, W3, Y)) :-
+    opp(X,Y), not(unsafe(state(V1, V2, V3, W1, Y, W3))),
+    writelist(['try W2 rides alone ',V1, V2, V3, W1, Y, W3, ' Boat:', Y]).
+
+move(state(V1, V2, V3, W1, W2, X, X), state(V1, V2, V3, W1, W2, Y, Y)) :-
+    opp(X,Y), not(unsafe(state(V1, V2, V3, W1, W2, Y))),
+    writelist(['try W3 rides alone ',V1, V2, V3, W1, W2, Y, ' Boat:', Y]).
+
+move(state(V1, V2, V3, W1, W2, W3, X), state(V1, V2, V3, W1, W2, W3, X)) :-
+    writelist([ 'BACKTRACK at:', V1, V2, V3, W1, W2, W3, X]), fail.
